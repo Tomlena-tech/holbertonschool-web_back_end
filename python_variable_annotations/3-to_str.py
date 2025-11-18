3ee366f16a91271d6a412d7e9ac570b3738fcdaa
Tomlena-tech <decourttthomas@gmail.com>
2025-11-18 10:54:09 +0100

    Add task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Basic annotations - to string"""
+def to_str(n: float) -> str:
+    """Convertit un nombre flottant en chaîne de caractères
+    Args:
+        n: nombre flottant à convertir
+    
+    Returns:
+        La représentation en chaîne de caractères de n
+    """
+    return str(n)
