71d9a7c2be30ab838fa994194934f41d78372e61
Tomlena-tech <decourttthomas@gmail.com>
2025-11-18 10:15:11 +0100

    Add task 1 completed

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""Basic annotations - concat"""
 
-""". Basic annotations - concat"""
-def concat(str1: str, str2: str) -> str: 
-    """"Concatene 2 chaines de caractères
-    Args str1 et str2
+
+def concat(str1: str, str2: str) -> str:
+    """Concatene 2 chaines de caractères
+    
+    Args:
+        str1: première chaîne
+        str2: deuxième chaîne
+    
+    Returns:
+        La concaténation de str1 et str2
     """
     return str1 + str2
