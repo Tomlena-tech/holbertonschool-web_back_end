60bb0152d83e9dd878a47020eb2d4c9b8b1d59a3
Tomlena-tech <decourttthomas@gmail.com>
2025-11-18 10:38:07 +0100

    Add task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic Annotations - floor"""
+
+def floor(n: float) -> int:
+    
+    """Return the whole part of float
+Args:
+        n: nombre flottant
+    
+    Returns:
+        La partie entière de n (arrondi vers le bas)
+    """
+    return int(n)
