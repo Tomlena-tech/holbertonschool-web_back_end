0860d1f3f6251c7205700841957695944d3f915e
Tomlena-tech <decourttthomas@gmail.com>
2025-11-24 10:55:45 +0100

    Add task 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Create one asynchrone way who's return random waiting"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
