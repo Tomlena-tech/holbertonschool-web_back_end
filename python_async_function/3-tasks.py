b9890f9d305c0daa79ca31087b6273a05e0d45fc
Tomlena-tech <decourttthomas@gmail.com>
2025-11-24 15:31:54 +0100

    Add task 3 wow

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""Create an asyncio Task for wait_random function"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Create a task for wait_random with max_delay"""
+    return asyncio.create_task(wait_random(max_delay))
+
