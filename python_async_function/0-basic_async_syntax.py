b9af56cabbce471199d4eaadc3716d0c35cba527
Tomlena-tech <decourttthomas@gmail.com>
2025-11-24 11:29:42 +0100

    Add def

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """ Wait for random delay """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+""" Execute multiple coroutines un the same time """
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+
+async def wait_n(n, max_delay):
+    n = 5,
+    max_delay = 5,
+    return va
+
