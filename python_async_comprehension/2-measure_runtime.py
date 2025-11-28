0e71b8e26535e9b7adaaa8f22d3abfb322976fee
Tomlena-tech <decourttthomas@gmail.com>
2025-11-28 10:37:52 +0100

    Add task 2 part 3

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -5,9 +5,17 @@ Asynchronous generator that yields random numbers
 import time
 import asyncio
 
-async_comprehension = __import__('async_comprehension').async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-async def async_comprehension()
-async def async_comprehension()
-async def async_comprehension()
-async def async_comprehension()
+
+async def measure_runtime():
+
+    start_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+    end_time = time.time()
+    return end_time - start_time
