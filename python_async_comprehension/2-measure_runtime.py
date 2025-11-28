5ca3928ab1b9892edaea4ee05049d150b9088968
Tomlena-tech <decourttthomas@gmail.com>
2025-11-28 09:46:59 +0100

    Add task 2 part 0

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Asynchronous generator that yields random numbers
+"""
+import time
+import asyncio
+
+from async_generator = __import__('0-async_generator').async_generator
