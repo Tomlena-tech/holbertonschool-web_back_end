fb91987491ec335be38d5b13434ca75bea17b825
Tomlena-tech <decourttthomas@gmail.com>
2025-12-01 21:27:33 +0100

    Add csv file / correction 1-main 3 last lines

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -8,16 +8,31 @@ from typing import List
 
 index_range = __import__('0-simple_helper_function').index_range
 
-def get_page(page: int, page_size: int)-> List[List]:
-    assert isinstance(page, int) and page > 0
-    assert isinstance(page_size, int) and page_size > 0
-    start, end = index_range(page, page_size)
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
     
-    dataset = []
-    with open("Popular_Baby_Names.csv") as f:
-        reader = csv.reader(f)
-        dataset = list(reader)
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a page of the dataset
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
         
-    return dataset [start : end]
-    
-  
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+        
+        return dataset[start:end]
