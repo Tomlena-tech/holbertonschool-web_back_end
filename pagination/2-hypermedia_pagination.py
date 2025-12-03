c0956832ba717fe0585a4319d1deb2b5bdb33e7c
Tomlena-tech <decourttthomas@gmail.com>
2025-12-03 11:20:18 +0100

    correct 'prev'

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -25,32 +25,29 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-    
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Get a page of the dataset
         """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
-        
+
         start, end = index_range(page, page_size)
         dataset = self.dataset()
-        
+
         return dataset[start:end]
-    
-    def get_hyper(self, page: int =1, page_size: int = 10) -> List[List]:
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         data = self.get_page(page, page_size)
         total_elements = len(self.dataset())
         total_pages = math.ceil(total_elements / page_size)
         next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > total_pages else None
-        
-        
-        return{
+        prev_page = page - 1 if page > 1 else None
+        return {
             'page_size': len(data),
             'page': page,
             'data': data,
             'next_page': next_page,
-            'rev_page': prev_page, 
+            'prev_page': prev_page,
             'total_pages': total_pages
         }
-        
