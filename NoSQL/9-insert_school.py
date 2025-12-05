57835523e166587d9e2814a0963ec14484c8c61c
Tomlena-tech <decourttthomas@gmail.com>
2025-12-05 13:42:36 +0100

    Add task 9 part 2

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -3,6 +3,4 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-    result = mongo_collection.insert_one(kwargs)
-    return result.inserted_id
-
+        return mongo_collection.insert_one(kwargs).inserted_id
