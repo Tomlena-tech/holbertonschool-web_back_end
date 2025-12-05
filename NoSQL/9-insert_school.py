4f4b51b40b569adacdd2e4be15aea2b2f64fb3eb
Tomlena-tech <decourttthomas@gmail.com>
2025-12-05 13:56:35 +0100

    Add task 9 part 3

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -3,4 +3,7 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-        return mongo_collection.insert_one(kwargs).inserted_id
+    """Insert a new document in a collection using kwargs
+    and return the new document's _id"""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
