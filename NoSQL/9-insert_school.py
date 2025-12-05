812bb0c1515bb1bddf64954fa5d777e1b0d33f49
Tomlena-tech <decourttthomas@gmail.com>
2025-12-05 13:58:01 +0100

    Add import ??

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ Insert a new document into a collection Kwargs"""
 
+from pymongo import MongoClient
+
+
 
 def insert_school(mongo_collection, **kwargs):
     """Insert a new document in a collection using kwargs
