64e254ab7e19c78ad5dab07494af2042a87a3f19
Tomlena-tech <decourttthomas@gmail.com>
2025-12-09 21:00:52 +0100

    Add task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+         this.length = length,
+        this.name = name,
+        this.students = students;
+    }
+    // Getter for name
+    get name () {
+        return this._name;
+    }
+    // Setter for name
+    set name (newName) {
+        if (typeof newName !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = newName;
+    }
+    // Getter for length
+    get length () {
+        return this._length;
+    }
+    // Setter for length
+    set length (newLength) {
+        if (typeof newLength !== 'number') {
+            throw new TypeError('Length must be a number');
+        }
+        this._length = newLength;
+    }
+    // Getter for students
+    get students () {
+        return this._students;
+    }
+    // Setter for students
+    set students (newStudents) {
+        if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        this._students = newStudents;
+    }
+}
