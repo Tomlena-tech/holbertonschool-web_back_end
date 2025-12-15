8a06b2c6b9a1e00f2c55dab7a8d4e117d2c96791
Tomlena-tech <decourttthomas@gmail.com>
2025-12-15 12:39:02 +0100

    Add task 0 with correct export default

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,7 @@
-const getListStudents = [
+export default function getListStudents() {
+  return [
     { id: 1, firstName : 'Guillaume', location : 'San Fransisco' },
     { id: 2, firstName : 'James', location :'Columbia'},
     { id: 5, firstName : 'Serena', location : 'San Fracisco'}
-]
+];
+}
