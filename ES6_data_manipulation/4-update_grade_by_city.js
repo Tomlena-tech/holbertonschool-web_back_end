3c2486f7a47f10cf4ef241eb746321234ebbddbf
Tomlena-tech <decourttthomas@gmail.com>
2025-12-15 15:06:33 +0100

    Add task 4 part 1

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,2 +1,11 @@
 export default function updateStudentGradeByCity (students, city, newGrades)
 {
+    return students.filter((student) => student.location === city)
+                   .map((student) => {
+                       const gradeRecord = newGrades.find((record) => record.studentId === student.id);
+                       if (gradeRecord) {
+                           return { ...student, grade: gradeRecord.grade };
+                       }
+                       return { ...student, grade: 'N/A' };
+                   });
+}
