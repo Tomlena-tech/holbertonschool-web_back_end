0c3edad17ef6d9ef99a4d58ea4e88a2fb24554ea
Tomlena-tech <decourttthomas@gmail.com>
2025-12-18 15:40:00 +0100

    Add correction parametre

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(fileName) {
-    return new Promise((resolve, reject) => {
+    return new Promise((reject) => {
         reject (new Error)(`Upload of ${fileName} failed`);
     });
 }
