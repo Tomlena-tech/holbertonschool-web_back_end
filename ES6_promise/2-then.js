2768b36103b94842540243fd8bd641b7502fa20c
Tomlena-tech <decourttthomas@gmail.com>
2025-12-18 11:09:45 +0100

    correct synthaxe

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,15 @@
 export default function handleResponseFromAPI(promise) {
-    promise.then((response => {
-        return {
-        status: 200,
-        body: 'success'
-        };
-    })).catch(error => {
-        return new Error();
-    })
-    .finally((() => {
-        console.log('Got response from API');
-    }));
+    promise
+        .then(() => {
+            return {
+                status: 200,
+                body: 'success'
+            };
+        })
+        .catch(() => {
+            return new Error();
+        })
+        .finally(() => {
+            console.log('Got response from API');
+        });
 }
