7e1f47b7738f7af64eb1757d5416174552cd1d61
Tomlena-tech <decourttthomas@gmail.com>
2025-12-18 13:13:41 +0100

    Add correct console.log

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -5,7 +5,7 @@ return Promise.all(allpromises)
 .then((value) => {
     const photo = value[0];
     const user = value[1];
-    console.log(`${user.firstName} ${user.lastName} has a profile photo named ${photo.body}`);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
 })
 .catch((error) => {
     console.log('Signup system offline');
