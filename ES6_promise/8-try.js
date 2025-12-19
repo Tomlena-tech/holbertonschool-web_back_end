071089f0a79a7a3947a75d9429a46e3cbb9b30d5
Tomlena-tech <decourttthomas@gmail.com>
2025-12-19 12:05:45 +0100

    Add task 8

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,4 @@
+export default function divideFunction(numerator, denominator) {
+     {if (denominator ===0) {throw (new Error("cannot divide be zero"));}
+        else {return (numerator / denominator);
+        }};}
