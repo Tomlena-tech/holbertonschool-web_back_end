c30bc66ca85bb9d97dad03ac8793ba762333304f
Tomlena-tech <decourttthomas@gmail.com>
2026-01-05 12:08:53 +0100

    Add task 1 part 0

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+function displayMessage() {
+    console.log("Welcome to Holberton School, what is your name?");
+}
+process.stdin.on('data', function(data) {
+    console.log("Your name is: " + data.toString().trim());
+
+
+process.stdin.on('end', function() {
+    console.log("This important software is now closing");
+});
+});
+displayMessage();
+
+
