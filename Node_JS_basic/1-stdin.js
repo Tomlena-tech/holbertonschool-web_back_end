27adbacaa7d739a9bb5b190609f8e9310134d260
Tomlena-tech <decourttthomas@gmail.com>
2026-01-05 12:30:54 +0100

    Add task 2 part 0

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 function displayMessage() {
-    console.log("Welcome to Holberton School, what is your name?");
+  console.log('Welcome to Holberton School, what is your name?');
 }
-process.stdin.on('data', function(data) {
-    console.log("Your name is: " + data.toString().trim());
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data.toString().trim()}`);
 
-
-process.stdin.on('end', function() {
-    console.log("This important software is now closing");
-});
+  process.stdin.on('end', () => {
+    console.log('This important software is now closing');
+  });
 });
 displayMessage();
-
-
